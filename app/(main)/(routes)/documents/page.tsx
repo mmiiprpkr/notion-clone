a931c04cdd8d7b5da6006b11abde488067d17d32
mmiiprpkr <mmiiprpkr@gmail.com>
2024-01-10 00:29:22 +0700

    1: auth

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/nextjs";
+import { PlusCircle } from "lucide-react";
+import Image from "next/image";
+
+const DocumentsPage = () => {
+   const { user } = useUser();
+   return ( 
+      <div className="h-full flex justify-center flex-col items-center space-y-4">
+         <Image 
+            src="/empty.png"
+            height="300"
+            width="300"
+            alt="Empty"
+            className="dark:hidden"
+         />
+         <Image 
+            src="/empty-dark.png"
+            height="300"
+            width="300"
+            alt="Empty"
+            className="hidden dark:block"
+         />
+         <h2 className="text-lg font-medium">
+            Welcome to {user?.firstName}&apos;s Jotion
+         </h2>
+         <Button>
+            <PlusCircle className="h-4 w-4 mr-2" />
+            Create a note
+         </Button>
+      </div>
+    );
+}
+ 
+export default DocumentsPage;
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+   providers: [
+     {
+       domain: "https://enabled-escargot-3.clerk.accounts.dev",
+       applicationID: "convex",
+     }
+   ]
+ }
